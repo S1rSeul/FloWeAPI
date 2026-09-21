4b5e8bab466ee02ecaa7c009dd5958505de79862
Artamonov <sirseul@gmail.com>
2026-09-21 14:35:21 +0400

    chore: инициализация структуры проекта

---
+++ src/main/java/com/floweapp/flowe_api/FloweApiApplication.java
@@ -0,0 +1,13 @@
+package com.floweapp.flowe_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloweApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloweApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/floweapp/flowe_api/FloweApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floweapp.flowe_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloweApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
