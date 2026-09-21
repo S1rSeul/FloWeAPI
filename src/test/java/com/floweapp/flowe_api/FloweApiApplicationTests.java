bc5d3b0a9896893b8f6b1f6551c103a7629d4875
Artamonov <sirseul@gmail.com>
2026-09-21 21:02:04 +0400

    edit, chore: переезд с resttemplate на mockmvc

--- src/test/java/com/floweapp/flowe_api/FloweApiApplicationTests.java
+++ src/test/java/com/floweapp/flowe_api/FloweApiApplicationTests.java
@@ -3,6 +3,10 @@ package com.floweapp.flowe_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 class FloweApiApplicationTests extends IntegrationTestBase {
 
@@ -10,4 +14,10 @@ class FloweApiApplicationTests extends IntegrationTestBase {
 	void contextLoads() {
 	}
 
+	@Test
+	void healthShouldReturnUp() throws Exception {
+		mockMvc.perform(get("/actuator/health"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("UP"));
+	}
 }
--- src/test/java/com/floweapp/flowe_api/IntegrationTestBase.java
+++ src/test/java/com/floweapp/flowe_api/IntegrationTestBase.java
@@ -1,15 +1,17 @@
 package com.floweapp.flowe_api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
+@AutoConfigureMockMvc
 public abstract class IntegrationTestBase {
 
     @Container
@@ -17,5 +19,5 @@ public abstract class IntegrationTestBase {
     static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:latest");
 
     @Autowired
-    protected TestRestTemplate restTemplate;
+    protected MockMvc mockMvc;
 }
