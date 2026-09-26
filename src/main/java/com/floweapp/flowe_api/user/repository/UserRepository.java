0f2ceed528d6f254e5e71a96b2eb7c25e9589dea
Daniel Kuzmin (PC) <kzm@dnlvld.ru>
2026-09-26 23:57:58 +0400

    Добавил репозиторий пользователя

--- src/main/java/com/floweapp/flowe_api/user/repository/UserRepository.java
+++ src/main/java/com/floweapp/flowe_api/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.floweapp.flowe_api.user.repository;
 
-public class UserRepository {
+import com.floweapp.flowe_api.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmailIgnoreCase(String email);
 }
