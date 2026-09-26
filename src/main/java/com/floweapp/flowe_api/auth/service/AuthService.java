641afbec7caf4d05282e65fa61b5f6c3a369ccf8
Daniel Kuzmin (PC) <kzm@dnlvld.ru>
2026-09-27 00:19:34 +0400

    Создал сервис регистрации пользователя

--- src/main/java/com/floweapp/flowe_api/auth/service/AuthService.java
+++ src/main/java/com/floweapp/flowe_api/auth/service/AuthService.java
@@ -1,4 +1,45 @@
 package com.floweapp.flowe_api.auth.service;
 
+import com.floweapp.flowe_api.auth.dto.RegisterRequest;
+import com.floweapp.flowe_api.auth.dto.RegisterResponse;
+import com.floweapp.flowe_api.user.entity.User;
+import com.floweapp.flowe_api.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.OffsetDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public RegisterResponse register(RegisterRequest request) {
+        String email = request.email().trim().toLowerCase();
+
+        if (userRepository.existsByEmailIgnoreCase(email)) {
+            throw new IllegalArgumentException("Пользователь с таким email уже существует");
+        }
+
+        User user = new User();
+        user.setEmail(email);
+        user.setPasswordHash(passwordEncoder.encode(request.password()));
+        user.setDisplayName(request.displayName().trim());
+        user.setCreatedAt(OffsetDateTime.now());
+
+
+        User savedUser = userRepository.save(user);
+        return new RegisterResponse(
+                savedUser.getId(),
+                savedUser.getEmail(),
+                savedUser.getDisplayName(),
+                savedUser.getCreatedAt()
+        );
+    }
 }
